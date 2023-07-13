ae2de8fe2193b938f34713e2efa39ec5d5a14e6c
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-13 20:40:48 +0100

    define main layout of the app

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
