0ab015daa7d22462a8c34c2d1e9b7c1368545850
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-14 18:06:33 +0100

    define additional types for supabase tables

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,3 +1,9 @@
 import { Database } from "./supabase";
 
 export type CabinType = Database["public"]["Tables"]["cabins"]["Row"];
+
+export type BookingType = Database["public"]["Tables"]["bookings"]["Row"];
+
+export type GuestType = Database["public"]["Tables"]["guests"]["Row"];
+
+export type SettingType = Database["public"]["Tables"]["settings"]["Row"];
