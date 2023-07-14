4257c850efc02353880a6adad0de8f92fb3239bd
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-14 21:02:23 +0100

    query all cabins and display them on cabins page

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,12 +1,19 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable></CabinTable>
+      </Row>
+    </>
   );
 }
 
