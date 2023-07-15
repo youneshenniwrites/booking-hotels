4f18c9f82178720366cdc6e33171150f79968df9
youneshenniwrites <youneshenniwrites@gmail.com>
2023-07-15 15:55:24 +0100

    add react hot toast

---
+++ src/ui/Toaster.tsx
@@ -0,0 +1,26 @@
+import { Toaster } from "react-hot-toast";
+
+export default function NotificationToaster() {
+  return (
+    <Toaster
+      position="top-center"
+      gutter={12}
+      containerStyle={{ margin: "8px" }}
+      toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 5000,
+        },
+        style: {
+          fontSize: "16px",
+          maxWidth: "500px",
+          padding: "16px 24px",
+          backgroundColor: "var(--color-grey-0)",
+          color: "var(--color-grey-700)",
+        },
+      }}
+    />
+  );
+}
